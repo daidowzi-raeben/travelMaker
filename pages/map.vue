8178f820442eb682c7af82222abd6b9cb4ef468b
장영욱 <raeben@naver.com>
2023-12-13 13:24:50 +0900

    fix

--- pages/map.vue
+++ pages/map.vue
@@ -1,47 +1,61 @@
 <template>
     <div>
-     <gmap-map :center="center" :zoom="12" style="width: 100%; height: 500px">
-          <gmap-cluster>
-            <gmap-marker v-for="(m, index) in markers"
-              :position="m.position"
-              :clickable="true" :draggable="true"
-              @click="center = m.position"
-              :key="index"
-              ></gmap-marker>
-          </gmap-cluster>
+
+        <!-- <gmap-place-input :default-place="place"@place_changed="setPlace">
+   </gmap-place-input> -->
+        <gmap-map ref="googleMap" :center="center" :zoom="12" style="width: 100%; height: 500px">
+            <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen"
+                @closeclick="infoWinOpen = false">
+            </gmap-info-window>
+            <gmap-cluster>
+                <gmap-marker v-for="(m, index) in EVENT_DATA.MAKERS" :key="index" :position="m.position" :clickable="true"
+                    :draggable="false" @click="onClickMaker(m, index)" :icon="makerImg">
+                </gmap-marker>
+            </gmap-cluster>
         </gmap-map>
+        {{ EVENT_DATA.MAKERS }}
     </div>
 </template>
 
 <script>
-// import { mapGetters, mapMutations } from 'vuex'
+import { mapState, mapMutations, mapActions } from 'vuex'
+
+// import imgpath from '~/static/images/logo.svg';
+const seoul = { lat: 37.5642135, lng: 127.0016985 };
+
 export default {
     name: 'MapContent',
     layout: 'index',
     data() {
         return {
-           center: {
-                lat: 37.5666612,
-                lng: 126.9783785
+            place: 'Singapore',
+            shape: {
+                coords: [10, 10, 10, 15, 15, 15, 15, 10],
+                type: 'poly'
             },
-            markers: [{
-                position: {
-                    lat: 10.0,
-                    lng: 10.0
+            makerImg: {
+                url: 'https://an2-img.amz.wtchn.net/image/v2/gL_DmC5m7rzEdBNnml8ybQ.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1KbklsMHNJbkFpT2lJdmRqSXZjM1J2Y21VdmFXMWhaMlV2TVRZNE1EVXhOVFk1TnpBNE9ERTBOakV3TnlJc0luRWlPamd3TENKM0lqb3hPVEl3ZlEuOEtVTGZueEJKRnllcUlSdzdjZVVCR2dwdjJ4dV9lR2xfVVpIWDNMSGNZcw',
+                scaledSize: { width: 20, height: 20 },
+            },
+            center: seoul,
+            // info window
+            infoWinOpen: false,
+            infoWindowPos: null,
+            currentMidx: null,
+            infoOptions: {
+                content: '',
+                pixelOffset: {
+                    width: 0,
+                    height: -35
                 }
-            }, {
-                position: {
-                    lat: 11.0,
-                    lng: 11.0
-                }
-            }]
+            },
         }
     },
     computed: {
-
+        ...mapState(['EVENT_DATA'])
     },
     created() {
-
+        this.ACTION_MAP_LIST()
     },
     mounted() {
         //         navigator.geolocation.getCurrentPosition((position) => {
@@ -53,7 +67,55 @@ export default {
         //     }
         // });
     },
+    methods: {
+        ...mapMutations(['MUTATIONS_MAP_LIST']),
+        ...mapActions(['ACTION_MAP_LIST']),
+
+        onClickMaker(v, e) {
+            console.log(v)
+            console.log(e)
+            this.$refs.googleMap.panTo({
+                lat: v.position.lat,
+                lng: v.position.lng
+            });
+
+            this.infoWindowPos = v.position;
+            this.infoOptions.content =
+                `<i>${v.detailData?.title}</i><br>
+            <img src="${v.detailData?.firstimage}" width="40">
+                `;
+
+            if (this.currentMidx === e) {
+                this.infoWinOpen = !this.infoWinOpen;
+            }
+            else {
+                this.infoWinOpen = true;
+                this.currentMidx = e;
+
+            }
+        }
+
+    }
+    ,
+    onClickCluster(v) {
+        console.log(v)
+    },
+    setPlaceText(place) {
+        this.place = place;
+    },
+    setPlace(place) {
+        this.latLng = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+        };
+    }
 }
 </script>
 
-<style lang="scss" ></style>
+<style lang="scss" >
+.gmnoprint  {
+    > .gm-svpc {
+        background:red !important;
+    }
+}
+</style>
